47d3257fc99a08a5d5bafed5ae7d15b31dee697f
Adeyemi Sadiq <adexsadiq01@gmail.com>
2024-01-31 10:36:27 +0100

    Fixed date and time format

--- frontend/src/components/VoterInfo.tsx
+++ frontend/src/components/VoterInfo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import moment from 'moment'; // Import Moment.js
+import { Box, Card, Divider, Typography } from '@mui/material';
 
-import { Box, Card, Divider, Typography } from '@mui/material'
 
 interface RowProps {
-  title: string
-  value: string | number
+  title: string;
+  value: string | number;
 }
 
 interface VoterInfoProps {
@@ -25,31 +26,29 @@ const Row: React.FC<RowProps> = ({ title, value }) => {
     </Box>
   );
 };
-const VoterInfo = ({
-  created_on,
-  end_time,
-  total_votes,
-}: VoterInfoProps) => {
+
+const VoterInfo = ({ created_on, end_time, total_votes }: VoterInfoProps) => {
   const voteInfo = [
-    { columName: 'Author', value: 'Tom' },
-    { columName: 'Start Date', value: (new Date(created_on)).toLocaleString() },
-    { columName: 'End Date', value: (new Date(end_time)).toLocaleString() },
-    { columName: 'Total votes', value: total_votes },
-    { columName: 'Algo Amounts', value: (0.2 * total_votes).toFixed(4) },
-  ]
+    { columnName: 'Author', value: 'Tom' },
+    { columnName: 'Start Date', value: moment(created_on).format('DD/MM/YYYY') },
+    { columnName: 'End Date', value: moment(end_time).format('DD/MM/YYYY') },
+    { columnName: 'Total votes', value: total_votes },
+    { columnName: 'Algo Amounts', value: (0.2 * total_votes).toFixed(4) },
+  ];
+
   return (
     <Card variant="outlined" sx={{ width: '100%', minHeight: 200, bgcolor: '#292f28', padding: 2 }}>
       <Typography variant="h6" fontWeight={'bold'} color={'#fff'} textAlign={'center'}>
         Vote Information
       </Typography>
       {voteInfo.map((item, index) => (
-        <Box>
-          <Row title={item.columName} value={item.value} key={`vote-info-row${index}`} />
+        <Box key={`vote-info-row${index}`}>
+          <Row title={item.columnName} value={item.value} />
           <Divider sx={{ bgcolor: '#eee' }} />
         </Box>
       ))}
     </Card>
-  )
-}
+  );
+};
 
-export default VoterInfo
+export default VoterInfo;
